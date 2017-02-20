feac9115e873a4cb2fbe9e40f736e7f7457fe87b
Travis Deshotels <travis.deshotels@gmail.com>
2017-02-20 09:24:13 -0600

    Add working API

--- src/main/java/net/ghostehsmells/rest/controller/QuoteController.java
+++ src/main/java/net/ghostehsmells/rest/controller/QuoteController.java
@@ -1,8 +1,10 @@
 package net.ghostehsmells.rest.controller;
 
-import java.util.List;
+import java.util.Map;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,13 +24,17 @@ public class QuoteController {
 	}
 
 	@RequestMapping(value="/", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-	public List<Quote> getQuotes(){
+	public Map<Integer, Quote> getQuotes(){
 		return quoteService.getAllQuotes();
 	}
 	
-	@RequestMapping()
-	public Quote getQuote(int id){
-		
-		return null;
+	@RequestMapping(value="/quote/{id}", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	public Quote getQuote(@PathVariable("id") int id){
+		return quoteService.getById(id);
+	}
+	
+	@RequestMapping(value="/quote", method=RequestMethod.POST, produces=MediaType.APPLICATION_JSON_VALUE)
+	public void saveQuote(@RequestBody Quote quote){
+		quoteService.addQuote(quote);
 	}
 }
--- src/main/java/net/ghostehsmells/service/QuoteServiceImpl.java
+++ src/main/java/net/ghostehsmells/service/QuoteServiceImpl.java
@@ -1,36 +1,39 @@
 package net.ghostehsmells.service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import net.ghostehsmells.beans.Quote;
 
-public class QuoteServiceImpl {//implements QuoteService{
+public class QuoteServiceImpl {
 
-	List<Quote> theQuotes;
+	Map<Integer, Quote> theQuotes;
+	int count;
 	
 	public QuoteServiceImpl() {
 		super();
-		theQuotes = new ArrayList<>();
-		theQuotes.add(new Quote("Zoe! Get yo ass back in the damn house! I'll deal with you later.", 1));
-		theQuotes.add(new Quote("HADOKEN!", 2));
+		theQuotes = new HashMap<>();
+		theQuotes.put(1, new Quote("Zoe! Get yo ass back in the damn house! I'll deal with you later.", 1));
+		theQuotes.put(2, new Quote("HADOKEN!", 2));
+		theQuotes.put(3, new Quote("Rip and Tear, until it is done!", 3));
+		theQuotes.put(4, new Quote("I don't make it a habit to fight the elderly", 4));
+		count = 4;
 	}
 
 	//@Override
-	public List<Quote> getAllQuotes() {
+	public Map<Integer, Quote> getAllQuotes() {
 		return theQuotes;
 	}
 
 	//@Override
 	public Quote getById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		return theQuotes.get(id);
 	}
 
 	//@Override
 	public void addQuote(Quote quote) {
-		// TODO Auto-generated method stub
-		
+		count++;
+		theQuotes.put(count, quote);
 	}
 
 }
