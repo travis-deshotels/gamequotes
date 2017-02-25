2094ad368cc3a12a5f9f95978031e428f66900a8
Travis Deshotels <travis.deshotels@gmail.com>
2017-02-25 15:24:41 -0600

    Add working frontend with post

--- src/main/resources/static/js/quotes.js
+++ src/main/resources/static/js/quotes.js
@@ -3,7 +3,13 @@ var app = angular.module('quoteApp',[]);
 app.controller('quoteCtrl', function($scope, $http) {
 	$scope.quotes=[];
 	$scope.quotesByGame=[];
-	$scope.gameText='';
+	$scope.quotesGame=[];
+	
+	$scope.newQuote={
+		"id": 0,
+		"quoteText": "",
+		"game": ""
+	};
 	
 	$scope.getQuotes = function() {
 		$http.get("api/quote").then(
@@ -14,14 +20,31 @@ app.controller('quoteCtrl', function($scope, $http) {
 		);
 	};
 	
-	$scope.getQuotesByGame = function() {
-		$http.get("api/quote/game/"+$scope.gameText).then(
-			//success
-			function(response){
-				$scope.quotesByGame = response.data;
+	$scope.getQuotesByGamePar = function(x) {
+		$http.get("api/quote/game/"+x).then(
+				//success
+				function(response){
+					$scope.quotesByGame = response.data;
+				}
+			);		
+	};
+	
+	$scope.saveQuote = function() {
+		$http.post("api/quote", $scope.newQuote).then(
+			function(){
+				//success
+				$scope.quotes.push({
+					"quoteText": $scope.newQuote.quoteText,
+					"game": $scope.newQuote.game
+				});
+				$scope.newQuote={
+					quoteText: '',
+					game: ''
+				};				
 			}
 		);
 	};
 	
+	//onload
 	$scope.getQuotes();
 });
